c9f39b217b585fef10160e1408976f2be29a8fef
OmegaM <omegamiao@gmail.com>
2016-05-09 23:36:25 +0800

    添加checkstyle支持

--- src/test/java/com/sigma/springgradle/demo/test/SpringTestCaseDemo.java
+++ src/test/java/com/sigma/springgradle/demo/test/SpringTestCaseDemo.java
@@ -4,20 +4,6 @@
 package com.sigma.springgradle.demo.test;
 
 
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
