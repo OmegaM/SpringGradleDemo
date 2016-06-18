58ab49ceb290b7f9bd66c82fa1d25f5a95756959
OmegaM <omegamiao@gmail.com>
2016-06-18 17:00:33 +0800

    编写测试代码

--- src/test/java/com/sigma/springgradle/demo/test/SpringTestCaseDemo.java
+++ src/test/java/com/sigma/springgradle/demo/test/SpringTestCaseDemo.java
@@ -3,11 +3,21 @@
  */
 package com.sigma.springgradle.demo.test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.sigma.springgradle.demo.controller.HomeController;
 
 /**
  * @author Omega
@@ -16,31 +26,25 @@ import org.testng.annotations.Test;
  *
  * @project SpringGradleDemo
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/root-context.xml",
+        "file:src/main/webapp/WEB-INF/spring-mvc-config.xml" })
+@WebAppConfiguration
 public class SpringTestCaseDemo {
+    private MockMvc mockMvc;
     
-    @BeforeMethod
-    public void setUp(){
-        
+    @Autowired
+    private HomeController homeController;
+    
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
     }
     
-    @AfterMethod
-    public void tearDown() throws Exception {
+    @org.junit.Test
+    public void testController() throws Exception {
+        this.mockMvc.perform(get("/index/home").contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isOk())
+                .andExpect(content().string("{\"message\":\"test jenkins hook!\"}"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
-    
-    @Test
-    public void test1() throws Exception {
-        Assert.assertTrue(true);
-    }
-    
-    @Test
-    public void test2() throws Exception {
-        Assert.assertTrue(true);
-    }
-    
-    @Test
-    public void test3() throws Exception {
-        Assert.assertTrue(true);
-    }
-    
 }
